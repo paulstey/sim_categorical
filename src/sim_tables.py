81095060c942a0c5a6cf05cf869cf8a7dc7c5059
paulstey <paulstey@gmail.com>
2016-12-26 16:53:19 -0500

    finished sim_student_courses() function

--- src/sim_tables.py
+++ src/sim_tables.py
@@ -1,6 +1,7 @@
 
 import pandas as pd 
 import numpy as np
+import json
 
 from xtabs2vars import *
 
@@ -11,109 +12,143 @@ PHD_PROGS = ['Anthropology', 'Applied Mathematics', 'Behavioral and Social Healt
   'Economics', 'Engineering', 'English', 'Epidemiology', 'History', 'Linguistics', 'Mathematics'\
   'Philosophy', 'Physics', 'Political Science', 'Psychology', 'Sociology']
 
-MA_PROGS = ['Biotechnology', 'Biomedical Engineering', 'Biostatistics', 'Computer Science', \
-  'Cybersecurity', 'Education', 'Engineering', 'English', 'Epidemiology', 'History', \
-  'Literary Arts', 'Medical Sciences', 'Physics', 'Political Science', 'Public Affairs', \
-  'Public Health', 'Social Analysis and Research', 'Theatre Arts and Performance Studies']
+MASTERS_PROGS = ['Applied Mathematics', 'Biotechnology', 'Biomedical Engineering', 'Biostatistics', \
+  'Behavioral and Social Health Sciences', 'Computer Science', 'Cybersecurity', 'Education', \
+  'Engineering', 'English', 'Epidemiology', 'History', 'Literary Arts', 'Physics', \
+  'Political Science', 'Public Affairs', 'Public Health', 'Theatre Arts and Performance Studies']
+
+MS_PROGS = ['Applied Mathematics', 'Behavioral and Social Health Sciences', 'Biotechnology', \
+  'Biomedical Engineering', 'Biostatistics', 'Computer Science', 'Cybersecurity', 'Engineering', \
+  'Epidemiology', 'Medical Sciences', 'Physics', 'Public Health', 'Social Analysis and Research']
+
+ETHNICITY = ['American Indian or Alskan Native', 'Asian', 'Black or African American', \
+    'Hispanic or Latino', 'Native Hawaiian or Pacific Islander', 'Race/Ethnicity Unknown', \
+    'Two or More Races', 'White']
+
+ETHNICITY_PROBS = [0.02, 0.26, 0.11, 0.09, 0.02, 0.09, 0.02, 0.39]
+
+CITIES = pd.read_csv('top_500_cities.csv')
+CITIES['probability'] = CITIES.iloc[:, 2]/sum(CITIES.iloc[:, 2])
+CITIES['citystate'] = CITIES['city'].map(str) + ', ' + CITIES['state']
+
 
 ## Final dataset columns:
 #
-# [1] left_program, degree_program(phd, ms), dept, undergrad_gpa, year_in_prog,
+# [1] left_program, degree_program(phd, ms), dept, undergrad_gpa, year_in_program,
 # [6] classes_taken, cohort_size, age, gender, international_student, 
 # [11] year_entered, num_publications, mean_impact_factor, max_impact_factor, 
 # [15] n_times_ta, funded_pos, distance_to_hometown, advisor_rank, lab_size, 
 # [20] is_married, has_children, undergrad_research_experience, num_grants
-# [24] num_ext_grants, 
+# [24] num_grants, 
 
 
 ## Tables to aggregate
 # students, grad_school_application, department, publications, ta_data, grants
 
 
-## professors_tbl:
+## professors_table:
 # prof_id, rank, lab_size, dept, 
 
-## publications_tbl:
+## publications_table:
 # student_id, impact_factor, with_advisor, 
 
-## students_tbl:
-# student_id, dept, fulltime, hometown, funded_position, year_in_prog, 
-# is_married, has_children, degree
+## STUDENTS_TABLE:
+# student_id, program, degree, fulltime, funded_position, year_in_program,
+# age, sex, ethnicity, hometown, marital_status, has_children 
 
-## grad_school_applications_tbl:
+## grad_school_applications_table:
 # student_id, undergrad_gpa, undergrad_institution, research_experience
 
-## GRANTS_TBL:
-# student_id, amount, funding_agency
+## GRANTS_TABLE:
+# grant_id, student_id, amount, funding_agency
 
-## ta_data_tbl:
+## ta_data_table:
 # student_id, course_id,
 
-## courses_tbl:
-# student_id, course_id, final_grade, semester
+## COURSES_TABLE:
+# id, student_id, course_id, final_grade
 
 
 def gen_student_ids(n):
-    ids = ['SN' + str(x) for x in range(1001000, 1001000 + n)]
+    ids = ['SR' + str(x) for x in range(1001000, 1001000 + n)]
     # np.random.shuffle(ids) 
     return ids
 
 def gen_age(yr):
-    '''This function simulates a student's age from year_in_prog'''
+    '''This function simulates a student's age from year_in_program'''
     age =  yr + 20 + np.random.choice([-1, 0, 1, 2, 3, 4, 5])
     return age
 
 
-def sim_students_table(student_ids, phd_progs, ma_progs):
-    ''' 
-    This function simulates the student_table. Note we make the naïve assumption 
-    that all degree programs are equally likely.
-    '''
-    n = len(student_ids)
-
+def sim_demographic_data(n, ethnicity, ethn_probs):
     married_children_xtab = pd.DataFrame()
-    married_children_xtab['married'] = [0.1, 0.2]
-    married_children_xtab['not married'] = [0.02, 0.68]
+    married_children_xtab['Married'] = [0.1, 0.18]
+    married_children_xtab['Single'] = [0.02, 0.70]
     married_children_xtab.index = ['has children', 'no children']
 
     xkids, xmarried = xtabs2vars(married_children_xtab, n)
     students = pd.DataFrame([xkids, xmarried]).transpose()
-    
-    students['sex'] = np.random.choice(["Male", "Female"], n)
-    students['ethnicity'] = np.random.choice(['American Indian or Alskan Native', \
-                                              'Asian', \
-                                              'Black or African American', \
-                                              'Hispanic or Latino', \
-                                              'Native Hawaiian or Pacific Islander', \
-                                              'Race/Ethnicity Unknown', \
-                                              'Two or More Races', \
-                                              'White'], 
-                                              n, p = [0.02, 0.26, 0.11, 0.09, 0.02, 0.09, 0.02, 0.39])
+    students.columns = ['has_children', 'marital_status']
 
-    students['degree'] = np.random.choice(['PhD', 'MA/MS'], n, p = [0.7, 0.3])
+    students['has_children'] = students['has_children'] == 'has children'
+    students['sex'] = np.random.choice(['Male', 'Female'], n)
+    students['ethnicity'] = np.random.choice(ethnicity, n, p = ethn_probs)
+    return students 
+
+sim_demographic_data(200, ETHNICITY, ETHNICITY_PROBS)
+
+
+def sim_students_table(n, ethnicity, ethn_probs, phd_progs, masters_progs, ms_progs, cities_df):
+    ''' 
+    This function simulates the student_table. Note we make the naïve assumption 
+    that all degree programs are equally likely.
+    '''
+    students = sim_demographic_data(n, ethnicity, ethn_probs)
+    
+    students['degree'] = np.random.choice(['PhD', 'MS'], n, p = [0.7, 0.3])
     students['funded_position'] = True
     students['fulltime'] = True
+    
     for i in range(n):
-        if students.loc[i, 'degree'] == "PhD":
-            students.loc[i, 'department'] = np.random.choice(phd_progs)
-            students.loc[i, 'year_in_prog'] = np.random.choice([1, 2, 3, 4, 5, 6, 7])
+        # PhD and masters students are hanlded quite differently, with 
+        # more nuances in the masters students data
+        if students.loc[i, 'degree'] == 'PhD':
+            students.loc[i, 'program'] = np.random.choice(phd_progs)
+            students.loc[i, 'year_in_program'] = np.random.choice([1, 2, 3, 4, 5, 6, 7])
         else:
-            students.loc[i, 'department'] = np.random.choice(ma_progs)
-            students.loc[i, 'year_in_prog'] = np.random.choice([1, 2, 3])
+            students.loc[i, 'program'] = np.random.choice(masters_progs)
+            if students.loc[i, 'program'] not in ms_progs:
+                students.loc[i, 'degree'] = 'MA'
+            students.loc[i, 'year_in_program'] = np.random.choice([1, 2, 3])
             
             # For masters students, we probabilistically assign 
             # funded and fulltime status (both are all TRUE for PhD)
-            students.loc[i, 'funded_position'] = np.random.choice([True, False], p = [0.3, 0.7])
             students.loc[i, 'fulltime'] = np.random.choice([True, False], p = [0.8, 0.2])
-        students.loc[i, 'age'] = gen_age(students.loc[i, 'year_in_prog'])
+            if not students.loc[i, 'fulltime']:
+                students.loc[i, 'funded_position'] = False 
+            else: 
+                students.loc[i, 'funded_position'] = np.random.choice([True, False], p = [0.3, 0.7])
+        students.loc[i, 'age'] = gen_age(students.loc[i, 'year_in_program'])
 
+    students['hometown'] = np.random.choice(cities_df['citystate'], n, p = cities_df['probability'])
+    students['student_id'] = gen_student_ids(n)
+    col_order = ['student_id', 'program', 'degree', 'fulltime', 'funded_position', 'year_in_program', \
+      'age', 'sex', 'ethnicity', 'hometown', 'marital_status', 'has_children']
+    
+    students = students[col_order]
     return students
 
-sim_students_table(range(20), PHD_PROGS, MA_PROGS)
+sim_students_table(200, ETHNICITY, ETHNICITY_PROBS, PHD_PROGS, MASTERS_PROGS, MS_PROGS, CITIES)
+
+
+def gen_grant_ids(n):
+    ids = ['F' + str(x) for x in range(50100, 50100 + n)]
+    # np.random.shuffle(ids) 
+    return ids
 
 
 def sim_grants_table(student_ids):
-    n = len(student_ids)
+    n = round(len(student_ids)/4)
 
     agencies = ['NIH', 'NSF', 'NIMH', 'DoD']
     amounts =  [x for x in range(3000, 10000, 500)] + [15000, 20000, \
@@ -125,14 +160,42 @@ def sim_grants_table(student_ids):
      0.045, 0.04, 0.035, 0.03, 0.025, 0.02, 0.019, 0.015, 0.01, 0.005, 0.001]
 
     df_out = pd.DataFrame()
-
-    df_out['grant_id'] = range(n)
-    df_out['student_id'] = student_ids
+    df_out['grant_id'] = gen_grant_ids(n)
+    df_out['student_id'] = np.random.choice(student_ids, n, replace = True)
     df_out['amount'] = np.random.choice(amounts, n, p = probs)
     df_out['agency'] = np.random.choice(agencies, n, p = [0.45, 0.35, 0.1, 0.1])
     return df_out
 
 
 sim_grants_table([1, 2, 3, 4, 5, 6])
+
+
+
+# This gets our course data from the JSON
+with open('dept_courses.json') as data_file:    
+    course_data = json.load(data_file)
+
+
+def gen_courses(prog, num_courses, course_data):
+    courses = np.random.choice(course_data[prog], num_courses, replace = False)
+    return courses 
+
+
+def sim_student_courses(row, course_data):
+    yrs = row.loc['year_in_program']
+    cnt = int(4 * np.log(5*yrs) + np.random.choice([-1, 0, 1, 2]))
+    df = pd.DataFrame()
+
+    df['student_id'] = [row.loc['student_id'] for i in range(cnt)]
+    df['course_num'] = gen_courses(row.loc['program'], cnt, course_data)
+    df['final_grade'] = np.random.choice(['A', 'B', 'C'], cnt, p = [0.56, 0.30, 0.14])
+
+    return df
+
+
+def sim_courses_table(student_df):
+
+
+gen_courses("Applied Mathematics", 5, course_data)
 
 
